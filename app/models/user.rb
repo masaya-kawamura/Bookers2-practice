a220806cce64941f1660b8a915c6df7175e93c41
masaya-kawamura <lucky.number0128@gmail.com>
2021-05-28 08:57:55 +0000

    [終了]いいね機能を実装しました

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,8 +8,9 @@ class User < ApplicationRecord
     validates :email, presence: true
     validates :introduction, length: {maximum: 50}
 
-    has_many :books, dependent: :destroy
-    has_many :comments, dependent: :destroy
+    has_many :books,    dependent: :destroy
+    has_many :comments,  dependent: :destroy
+    has_many :favorites, dependent: :destroy
 
     attachment :profile_image
 end
